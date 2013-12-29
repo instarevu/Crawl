d29479f43cc880e9e49e4198a9cf5d51597e72aa
ir <instarevu@gmail.com>
2013-12-29 15:34:32 -0800

    Updated & fixed configs

--- config/es/scripts/dev/bootstrapindex.sh
+++ config/es/scripts/dev/bootstrapindex.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
+set -o xtrace
+
 server=http://localhost:9200
+index=amazon
+types=(item exitem)
 
 # DELETE INDEX
-echo 'Deleting Index Amazon....'
-sleep 30
-curl -XDELETE ${server}'/amazon/'
-
+echo 'Deleting Index.... : '${index}
+#sleep 30
+curl -XDELETE ${server}'/'${index}'/'
 
 
 # CREATE INDEX
-echo 'Creating Index Amazon....'
-curl -XPUT ${server}'/amazon'
+echo 'Creating Index.... : '${index}
+curl -XPUT ${server}'/'${index}  --data @../../config/index/${index}.json
 
-echo 'Updating Mapping for type:item'
-curl -XPUT ${server}'/amazon/item/_mapping' --data @../../config/mappings/item.json
+#curl -XPOST ${server}'/'${index}'/_close'
+#echo 'Update settings for  Index.... : '${index}
+#curl -XPUT ${server}'/'${index}'/_settings'  --data @../../config/index/${index}.json
+#curl -XPOST ${server}'/'${index}'/_open'
+
+for type in "${types[@]}"
+do
+    echo 'Updating Mapping for type: '${type}
+    curl -XPUT ${server}'/'${index}'/'${type}'/_mapping' --data @../../config/type/${type}.json
+done
